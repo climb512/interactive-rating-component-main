3c72d59f3af556d63d2318a0f7ccbe877b1fa4cf
Ted <climb512@gmail.com>
2023-06-16 00:16:45 +0700

    got form all working. 95% done.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ function formHandler() {
 
     for (i = 0; i < ele.length; i++) {
         if (ele[i].checked) {
+            const val = ele[i].value;
             document.getElementById("rating-state").style.display="none";
             document.getElementById("thank-you-state").style.display="flex";
+            document.getElementById("submitted-rating").innerHTML=val;
         }
     }
 }
